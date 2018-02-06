013576cfba45643ac3e19b88e8f363b9f4e38d40
Loreleine Le Clerc <loreleine.leclerc@decathlon.com>
2018-02-06 16:00:43 +0100

    creater header with a f** menu that is lame

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Profile from "./modules/profile/profile.js"
+import Profile from "./modules/profile/profile.js";
+import Header from './modules/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Profile />
+        <Header />
       </div>
     );
   }
