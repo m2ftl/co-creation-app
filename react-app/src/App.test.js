e0f0033559098718f30ec620f2861148d3502148
FABIEN GLORY <fabien.glory@decathlon.com>
2018-02-06 18:23:13 +0100

    add provider on App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
-
+import {Provider} from 'react-redux';
+import store from "./store/store"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>, div);
   </BrowserRouter>
   , div);
   ReactDOM.unmountComponentAtNode(div);
