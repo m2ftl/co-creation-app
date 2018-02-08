c2d3fc9315746f24543f1aa9047e841bfea83155
FABIEN GLORY <fabien.glory@decathlon.com>
2018-02-08 11:20:46 +0100

    app.test fixed

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
-import store from "./store/store"
+import { store } from "./store/store";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
