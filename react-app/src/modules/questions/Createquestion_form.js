b2627ed014360481a67484e8c6f25c364457af8e
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2018-02-08 08:44:31 +0100

    question

--- react-app/src/modules/questions/Createquestion_form.js
+++ react-app/src/modules/questions/Createquestion_form.js
@@ -59,37 +59,37 @@ class Createquestionform extends Component{
               <label  className="topTitle" >Select the level of destination</label>
               <div>
               <label> Beginner </label>
-              <Field name="level.Beginner" id="Beginner" component="input" type="checkbox"  />
+              <Field name="topic.Beginner" id="Beginner" component="input" type="checkbox"  />
               <label className="ml-5"> Advanced </label>
-              <Field name="level.Advanced" id="Advanced" component="input" type="checkbox"  />
+              <Field name="topic.Advanced" id="Advanced" component="input" type="checkbox"  />
               <label className="ml-5"> Expert </label>
-              <Field name="level.Expert" id="Expert" component="input" type="checkbox"  />
+              <Field name="topic.Expert" id="Expert" component="input" type="checkbox"  />
               </div>
               <label  className="topTitle" >Select the index of destination</label>
               <div>
               <label> Category 1: to 4.4</label>
-              <Field name="category.Category1" id="Category1" component="input" type="checkbox"  />
+              <Field name="topic.Category1" id="Category1" component="input" type="checkbox"  />
               <label className="ml-5"> Category 2: from 4.4 to 11.4 </label>
-              <Field name="category.Category2" id="Category2" component="input" type="checkbox"  />
+              <Field name="topic.Category2" id="Category2" component="input" type="checkbox"  />
               <label className="ml-5"> Category 3: from 11.4 to 18.4 </label>
-              <Field name="category.Category3" id="Category 3" component="input" type="checkbox"  />
+              <Field name="topic.Category3" id="Category 3" component="input" type="checkbox"  />
               <label className="ml-5"> Category 4: from 18.4 to 26.4 </label>
-              <Field name="category.Category4" id="Category4" component="input" type="checkbox"  />
+              <Field name="topic.Category4" id="Category4" component="input" type="checkbox"  />
               <label className="ml-5"> Category 5: from 26.4 to 36 </label>
-              <Field name="category.Category5" id="Category5" component="input" type="checkbox"  />
+              <Field name="topic.Category5" id="Category5" component="input" type="checkbox"  />
               <label className="ml-5"> Category 6: from 36 to 54 </label>
-              <Field name="category.Category6" id="Category6" component="input" type="checkbox"  />
+              <Field name="topic.Category6" id="Category6" component="input" type="checkbox"  />
               </div>
               <label  className="topTitle" >Select the weather of destination</label>
               <div>
               <label> Rain </label>
-              <Field name="weather.Rain" id="Rain" component="input" type="checkbox"  />
+              <Field name="topic.Rain" id="Rain" component="input" type="checkbox"  />
               <label className="ml-5"> Cold </label>
-              <Field name="weather.Cold" id="Cold" component="input" type="checkbox"  />
+              <Field name="topic.Cold" id="Cold" component="input" type="checkbox"  />
               <label className="ml-5"> Mild </label>
-              <Field name="weather.Mild" id="Mild" component="input" type="checkbox"  />
+              <Field name="topic.Mild" id="Mild" component="input" type="checkbox"  />
               <label className="ml-5"> Sunny </label>
-              <Field name="weather.Sunny" id="Sunny" component="input" type="checkbox"  />
+              <Field name="topic.Sunny" id="Sunny" component="input" type="checkbox"  />
               </div>
 
         </div>
@@ -106,18 +106,14 @@ let createReduxForm = reduxForm({
   form: 'createquestion',
   validate,
   initialValues: {
-    level: {
+    topic: {
       Beginner: true,
       Advanced: true,
-      Expert: true
-    },
-    weather: {
+      Expert: true,
       Rain: true,
       Cold: true,
       Mild: true,
-      Sunny: true
-    },
-    category: {
+      Sunny: true,
       Category1: true,
       Category2: true,
       Category3: true,
--- server.js
+++ server.js
@@ -44,9 +44,7 @@ app.post('/createquestionnew', function(req, res) {
     ssl: true,
   });
 
-  const cat = Object.keys(req.body.category).filter(key => req.body.category[key]===true );
-  const lev = Object.keys(req.body.level).filter(key => req.body.level[key]===true );
-  const weath = Object.keys(req.body.weather).filter(key => req.body.weather[key]===true );
+  const top = Object.keys(req.body.topic).filter(key => req.body.topic[key]===true );
 
   client.connect();
   client.query("INSERT INTO questions (id, title, description,status,date,id_owner) VALUES (uuid_generate_v4(),$1,$2,'open',Now(),$3) RETURNING *", [req.body.title, req.body.description,req.body.uuid],
@@ -54,19 +52,18 @@ app.post('/createquestionnew', function(req, res) {
     if(error){
       console.warn(error);
       res.send({result:"failed"});
-    }
-    weath.forEach(function(element) {
-      client.query("INSERT INTO question_weather (id_question, id_weather) VALUES ($1,$2)", [res1.rows[0].id,element])
-    });
-    lev.forEach(function(element) {
-      client.query("INSERT INTO question_level (id_question, id_level) VALUES ($1,$2)", [res1.rows[0].id,element])
-    });
-    cat.forEach(function(element) {
-      client.query("INSERT INTO question_index_category (id_question, id_index_category) VALUES ($1,$2)", [res1.rows[0].id,element])
-    });
+    } else {
+    top.forEach(function(element) {
+      client.query("INSERT INTO question_topic (id_question, topic) VALUES ($1,$2)", [res1.rows[0].id,element],function(error, res1){
+        if(error)
+          { console.warn(error);
+            res.send({result:"failed"})}
+      })
+    })}
     res.send({result:"success"});
+  })
 });
-});
+
 
 app.get('/viewideasall', function(req, res) {
   const client = new PG.Client({
