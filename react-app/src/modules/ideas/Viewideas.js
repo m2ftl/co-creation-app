0b9f76a56411a58b3b8a8034cef5ab13f7fb2f48
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2018-02-08 11:37:36 +0100

    like

--- react-app/src/modules/ideas/Viewideas.js
+++ react-app/src/modules/ideas/Viewideas.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "../../App.css";
 import { connect } from 'react-redux';
-import { getIdeas } from "../../store/ideas/selectors"
+import getIdeas from "../../store/ideas/selectors";
 import ideasActions from '../../store/ideas/actions';
 
 
