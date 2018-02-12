0de4f4f11c27fe95369e6e46280d756d872bb5ed
Maxime Delplanque <maxime.delplanque@decathlon.com>
2018-02-12 13:38:10 +0100

    Add picture and welcoming user

--- react-app/src/modules/header/HeaderNavBar.js
+++ react-app/src/modules/header/HeaderNavBar.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import '../../App.css';
 import inesis_logo2 from '../../images/inesis_logo2.png';
 import user_pic from '../../images/user_pic.png';
+import {getUserState} from '../../store/user/selectors';
+import { connect } from 'react-redux';
 
-export default class HeaderNavBar extends Component {
+class HeaderNavBar extends Component {
   render(){
+    console.log();
     return(
+
         <div className="container-grid-navbar">
           <div className="item-navbar_inesis">
             <img src={inesis_logo2} className="inesis_logo" alt="logo" width={'150px'} />
           </div>
-          <div className="item-navbar">
-            <img src={user_pic} className="user_pic" alt="logo" width={'50px'} />
-            <span>Welcome User</span>
+          <div className="item-navbar_user">
+            <div><img src={this.props.googleUser.avatar} className="user_pic" /></div>
+            <div><span>Welcome {this.props.googleUser.givenName}</span></div>
           </div>
         </div>
     );
   }
 }
+
+export default connect(getUserState)(HeaderNavBar);
