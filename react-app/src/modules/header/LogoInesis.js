9c342410b73477de5b0823c3ab5bffb2af4fddb6
Maxime Delplanque <maxime.delplanque@decathlon.com>
2018-02-14 14:17:11 +0100

    add logo_inesis_redirection_dashboard

--- react-app/src/modules/header/LogoInesis.js
+++ react-app/src/modules/header/LogoInesis.js
@@ -3,13 +3,14 @@ import '../../App.css';
 import inesis_logo2 from '../../images/inesis_logo2.png';
 import {getUserState} from '../../store/user/selectors';
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 
 class LogoInesis extends Component {
   render(){
     return(
         <div className="container-grid-navbar">
           <div className="item-navbar_inesis">
-            <img src={inesis_logo2} className="inesis_logo" alt="logo" width={'150px'} />
+            <Link to={'/dashboard'}><img src={inesis_logo2} className="inesis_logo" alt="logo" width={'150px'} /></Link>
           </div>
         </div>
     );
