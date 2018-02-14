e20c0d412404210e9c098b8fb1183ecfca6c3708
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2018-02-14 11:34:51 +0100

    question

--- react-app/src/modules/questions/Question.js
+++ react-app/src/modules/questions/Question.js
@@ -15,7 +15,7 @@ class Question extends Component {
   componentDidMount() {
     if(this.props.questions.length === 0) {
       // TODO: fetch only this idea
-      this.props.retrieveQuestions();
+      this.props.retrieveQuestions(this.props.useruuid);
     }
   }
 
@@ -47,16 +47,20 @@ class Question extends Component {
       <div className="list_ideas_block">
         <h1>Question details</h1>
         <div className="idea_item">
-          <h3>{found_question.title}</h3>
+          <h3 className="mt-5">{found_question.title}</h3>
           <div className="idea_description">
             <div>{found_question.description}</div>
-            <span>submitted by {found_question.first_name} {found_question.last_name}</span>
+            <div className="mt-5">submitted by {found_question.first_name} {found_question.last_name}</div>
           </div>
         </div>
         <form onSubmit={this.onSubmit}>
-          <label>Write your answer</label>
-          <input type="text" onChange={this.handleInput}/>
-          <button type="submit" className="btn">Send</button>
+          <div> Write your answer</div>
+          <div className="question_answer">
+          <textarea className="form-control formset" onChange={this.handleInput}/>
+          </div>
+          <div>
+          <button type="submit" className="btn mt-2">Send</button>
+          </div>
         </form>
       </div>
     );
