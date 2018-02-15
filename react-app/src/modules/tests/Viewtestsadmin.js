70deadfea6443d548c2e6299444ae2faf2bc7d45
FABIEN GLORY <fabien.glory@decathlon.com>
2018-02-15 09:24:42 +0100

    filter opened / closed

--- react-app/src/modules/tests/Viewtestsadmin.js
+++ react-app/src/modules/tests/Viewtestsadmin.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import getTests from "../../store/tests/selectors";
 import testsActions from '../../store/tests/actions';
 import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class ViewTestsadmin extends Component {
 
@@ -12,8 +13,8 @@ class ViewTestsadmin extends Component {
   }
 
   render() {
-    let listTestsadmin = this.props.testsadmin.map((test, index) => {
-      if(this.props.testsadmin.length !== 0) {
+    let listTestsOpened = this.props.testsadmin.map((test, index) => {
+      if(this.props.testsadmin.length !== 0 && test.status === "open") {
         let format_date = new Date(test.date);
         let formated_date = format_date.getDate()+'/'+(format_date.getMonth()+1)+'/'+format_date.getFullYear();
 
@@ -25,7 +26,6 @@ class ViewTestsadmin extends Component {
               </div>
               <h3>{test.title}</h3>
               <div className="viewtest_item_date">submitted: &nbsp;{formated_date}</div>
-              <div className="viewtest_item_date">{test.status}</div>
               <Link to={"/viewtestadmin/"+test.id}>
                 <div className="viewtest_item_button">Test Report</div>
               </Link>
@@ -38,15 +38,53 @@ class ViewTestsadmin extends Component {
           </div>
         )
       } else {
-      return (<div>Sorry, there is no test for the moment</div>)
+      return (<div></div>)
     }
   });
+
+  let listTestsClosed = this.props.testsadmin.map((test, index) => {
+    if(this.props.testsadmin.length !== 0 && test.status === "closed") {
+      let format_date = new Date(test.date);
+      let formated_date = format_date.getDate()+'/'+(format_date.getMonth()+1)+'/'+format_date.getFullYear();
+
+      return (
+        <div key={index} className="viewtest_item_bckgrd">
+          <div className="viewtest_item_content">
+            <div className="product_picture">
+              <img src={""+test.picture+""} alt="product_picture" />
+            </div>
+            <h3>{test.title}</h3>
+            <div className="viewtest_item_date">submitted: &nbsp;{formated_date}</div>
+            <Link to={"/viewtesxtadmin/"+test.id}>
+              <div className="viewtest_item_button">Test Report</div>
+            </Link>
+            <form onSubmit={(e)=> {
+              e.preventDefault();
+              this.props.archiveTest(test.id)}}>
+            <button type="submit">Archive Test</button>
+            </form>
+          </div>
+        </div>
+      )
+    } else {
+    return (<div></div>)
+  }
+});
 
 
     return (
       <div className="ideas_block">
         <h1>Tests Reports</h1>
-        <div className="div_glob_tests">{listTestsadmin}</div>
+        <div className="div_glob_tests">
+          <Tabs>
+            <TabList>
+              <Tab>Opened Tests</Tab>
+              <Tab>Closed Tests</Tab>
+            </TabList>
+            <TabPanel>{listTestsOpened}</TabPanel>
+            <TabPanel>{listTestsClosed}</TabPanel>
+          </Tabs>
+        </div>
       </div>
     );
   }
